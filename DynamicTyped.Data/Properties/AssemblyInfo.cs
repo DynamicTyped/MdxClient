93f6abab6c403b346a0969e25455788563b48608
Rick Bundren <bundy845@gmail.com>
2012-11-07 10:49:45 -0500

    added version

--- DynamicTyped.Data/Properties/AssemblyInfo.cs
+++ DynamicTyped.Data/Properties/AssemblyInfo.cs
@@ -6,9 +6,9 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("DynamicTyped.Data")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Specialized ADOMD DB provider")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("DynamicTyped")]
 [assembly: AssemblyProduct("DynamicTyped.Data")]
 [assembly: AssemblyCopyright("Copyright ©  2012")]
 [assembly: AssemblyTrademark("")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1211.11107.0")]
+[assembly: AssemblyFileVersion("1.1211.11107.0")]
