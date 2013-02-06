facb6a3d677d722190a6fec4485f7f344af72bcb
Rick Bundren <bundy845@gmail.com>
2013-02-06 09:40:54 -0500

    Version number

--- MdxClient/Properties/AssemblyInfo.cs
+++ MdxClient/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.1301.20115.0")]
-[assembly: AssemblyFileVersion("1.1301.20115.0")]
+[assembly: AssemblyVersion("1.1302.20206.0")]
+[assembly: AssemblyFileVersion("1.1302.20206.0")]
